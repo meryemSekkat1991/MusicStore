d29c6b97cf882d92c3f3edc259ac5036571f8c6a
meryemSekkat <meryemsekkat1991@gmail.com>
2022-12-15 16:31:32 +0100

    feat: songs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_130433) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_152239) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,15 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_130433) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "singers", force: :cascade do |t|
-    t.string "name"
-    t.string "lastname"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "songs", force: :cascade do |t|
-    t.string "title"
+    t.text "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
