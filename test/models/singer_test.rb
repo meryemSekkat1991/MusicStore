83ad24b3393c837fe35ab5f97fed58cb9052d3a5
meryemSekkat <meryemsekkat1991@gmail.com>
2023-01-16 15:34:50 +0100

    session

--- test/models/singer_test.rb
+++ test/models/singer_test.rb
@@ -4,4 +4,32 @@ class SingerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @singer = Singer.new(name: "Sports")
+  end
+
+  test "singer should be valid" do
+    assert @singer.valid?
+  end
+
+  test "name should be present" do
+    @singer.name = " "
+    assert_not @singer.valid?
+  end
+
+  test "name should be unique" do
+    @singer.save
+    @singer2 = singer.new(name: "Sports")
+    assert_not @singer2.valid?
+  end
+
+  test "name should not be too long" do
+    @singer.name = "a" * 26
+    assert_not @singer.valid?
+  end
+
+  test "name should not be too short" do
+    @singer.name = "aa"
+    assert_not @singer.valid?
+  end
 end
